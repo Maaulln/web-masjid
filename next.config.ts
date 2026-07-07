5f04141b4560584ebf0077606d281a00ab057792
Maaulln <irawan@it.student.pens.ac.id>
2026-07-07 10:32:27 +0700

    fix: remove invalid eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Mematikan error lint saat build di Vercel
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Mematikan error type check saat build di Vercel (opsional)
     ignoreBuildErrors: true,
